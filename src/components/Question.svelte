ab953dfb3b7fd7d5e37f89c7d50225b05cf92d2e
Sean Welch <sean.welch@me.com>
2023-03-17 11:26:45 -0400

    Basic component with logic

---
+++ src/components/Question.svelte
@@ -0,0 +1,21 @@
+<script>
+	let expanded = false;
+	export let title;
+	export let detail;
+
+	function toggleExpanded() {
+		expanded = !expanded;
+	}
+</script>
+
+<div class="title" on:click={toggleExpanded}>
+	<h3>{title}</h3>
+	{#if detail}
+		<div class="detail">
+			<p>{detail}</p>
+		</div>
+	{/if}
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Question from '../components/Question.svelte';
+</script>
+
+<div class="page-wrapper">
+	<Question
+		title="Who is Joe Mallory"
+		detail="Probably best described as a humble and light-spirited digital Chimera, theres no industry buzz-word label that fits Joe. And thats just fine with him."
+	/>
+</div>

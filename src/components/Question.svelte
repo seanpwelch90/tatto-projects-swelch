cca1a3863038114c0bb70c7f8217defd77304d12
Sean Welch <sean.welch@me.com>
2023-03-17 15:23:01 -0400

    Styling almost complete, cleanup

--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -1,21 +1,58 @@
 <script>
+	import { slide } from 'svelte/transition';
+
+	export let title = '';
+	export let detail = '';
 	let expanded = false;
-	export let title;
-	export let detail;
 
 	function toggleExpanded() {
 		expanded = !expanded;
 	}
 </script>
 
-<div class="title" on:click={toggleExpanded}>
-	<h3>{title}</h3>
-	{#if detail}
-		<div class="detail">
-			<p>{detail}</p>
+<div class={$$props.class}>
+	<div class="question-wrapper">
+		<div
+			class="title-wrapper"
+			style="display: flex; justify-content: space-between; align-items: baseline"
+		>
+			<h3 on:click={toggleExpanded}>{title}</h3>
+			<button class="expand_button" on:click={toggleExpanded}>
+				{expanded ? '↓' : '→'}
+			</button>
 		</div>
-	{/if}
+		{#if expanded}
+			<div transition:slide|local class="detail">
+				<p>{detail}</p>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <style>
+	.detail p {
+		font-size: 12px;
+    line-height: 1.8;
+	}
+	.detail {
+    margin: 20px 40px 20px;
+	}
+	.title-wrapper h3 {
+		font-size: 14px;
+		color: #608bc5;
+		font-weight: 600;
+    cursor: pointer;
+	}
+	.expand_button {
+		width: 35px;
+		height: 35px;
+		border-radius: 100%;
+    color: #608bc5;
+		background-color: #ffffff;
+    border: 1px solid #608bc5;
+	}
+	.question-wrapper {
+		padding-top: 15px;
+		padding-bottom: 15px;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,48 @@
 </script>
 
 <div class="page-wrapper">
-	<Question
-		title="Who is Joe Mallory"
-		detail="Probably best described as a humble and light-spirited digital Chimera, theres no industry buzz-word label that fits Joe. And thats just fine with him."
-	/>
+	<div class="question-wrapper">
+		<Question
+			class="question"
+			title="Who is Joe Mallory"
+			detail="Probably best described as a humble and light-spirited digital Chimera, theres no industry buzz-word label that fits Joe. And thats just fine with him."
+		/>
+		<Question
+			class="question"
+			title="Who is Joe Mallory"
+			detail="Probably best described as a humble and light-spirited digital Chimera, theres no industry buzz-word label that fits Joe. And thats just fine with him."
+		/>
+		<Question
+			class="question"
+			title="Who is Joe Mallory"
+			detail="Probably best described as a humble and light-spirited digital Chimera, theres no industry buzz-word label that fits Joe. And thats just fine with him."
+		/>
+	</div>
 </div>
+
+<style>
+	:global(body) {
+		background: linear-gradient(to bottom, #ffffff 50%, #f6f8fc 50%);
+	}
+	:global(*) {
+		margin: 0;
+		padding: 0;
+	}
+	.question-wrapper {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		padding-left: 40px;
+		padding-right: 40px;
+		max-width: 80vw;
+		box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+		border-radius: 20px;
+		background-color: #ffffff;
+		margin: 45px auto auto;
+	}
+
+	.question-wrapper :global(.question:not(:last-child)) {
+		border-bottom: 1px solid #608bc5;
+	}
+	.page-wrapper {
+		min-height: 100vh;
+	}
+</style>
